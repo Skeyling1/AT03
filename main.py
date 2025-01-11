eb55247be2c49ed65c0c19d057c1e229d5553a01
Skeyling1 <skeyling1@gmail.com>
2025-01-11 17:02:11 +0300

    fff

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 
+import requests
+
+def get_weather(api_key, city):
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+def get_github_user(username):
+    url = f'https://api.github.com/users/{username}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
--- test.py
+++ test.py
@@ -1,4 +1,34 @@
 import pytest
 from main import get_weather
 
+def test_get_weather(mocker):
+    mock_get = mocker.patch('main.requests.get')
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.json.return_value = {
+        'weather' : [{'description': 'clear sky'}],
+        'main' : {'temp': 273.15}
+    }
+
+    api_key = '63b52308593f20bc73d96a66df4e7dfa'
+    city = 'London'
+
+    weather_data = get_weather(api_key, city)
+
+    assert weather_data == {
+        'weather': [{'description': 'clear sky'}],
+        'main': {'temp': 273.15}
+    }
+
+def test_get_weather_with_error(mocker):
+    mock_get = mocker.patch('main.requests.get')
+    mock_get.return_value.status_code = 404
+
+    api_key = '63b52308593f20bc73d96a66df4e7dfa'
+    city = 'London'
+
+    weather_data = get_weather(api_key, city)
+    assert weather_data == None
+
+
+
 
